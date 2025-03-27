3c8254ace799781cafed4934d5f33282076ecdc8
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-03-27 11:51:37 -0300

    feat: add termos

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,18 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack screenOptions={{ 
-            headerShown: true,
-            headerStyle: {
-                backgroundColor: '#3b5998',
-            }, 
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                fontWeight: 'bold',
-            }
-        }}>
-            <Stack.Screen name="index" options={{ title: 'Home'}}/>
-            <Stack.Screen name="about" options={{ title: 'Sobre'}}/>
+        <Stack>
+            <Stack.Screen name="index" options={{ title: 'Home' }} />
+            <Stack.Screen name="sobre" options={{ title: 'Sobre' }} />
+            <Stack.Screen name="(aux)/termos" options={{ title: 'Termos de uso' }} />
         </Stack>
     )
     }
--- app/index.jsx
+++ app/index.jsx
@@ -1,62 +1,38 @@
-import { View, Text, StyleSheet, Button } from "react-native";
-import { Link, Stack, useNavigation } from "expo-router";
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Home() {
-  const navigation = useNavigation();
-
-
-  const hideHeader = () => {
-    navigation.setOptions({
-      headerShown: false,
-    });
-  }
-  const showHeader = () => {
-    navigation.setOptions({
-      headerShown: true,
-    });
-  }
-  return (
-    <View style={styles.container}>
-      <Stack.Screen options={{ 
-        title: 'SENAI', 
-        headerRight: () => (
-          <Button onPress={() => alert("Olá aluno de DS")} title="info" color="red"/>
-        ),
-        headerStyle: {
-          backgroundColor: 'black',
-        },
-        headerTintColor: 'red',
-        }}/>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Bem-vindo à tela inicial!</Text>
-      </View>
-      
-      <Link href="/sobre">Ir para Sobre</Link>
-      <Button title="Ocultar header" onPress={hideHeader}/>
-      <Button title="Mostrar header" onPress={showHeader}/>
+  return(
+    <View>
+      <Link href="/sobre" asChild>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Ir para tela sobre</Text>
+      </Pressable>
+      </Link>
+      <Link href="/termos">Leia os Termos</Link>
     </View>
-  );
-};
-
+  )
+}
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-  titleContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 414,
-    height: 200,
-    backgroundColor: 'black',
-    borderWidth: 3,
-    borderColor: 'red',
-    borderRadius: 8,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
   },
   title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: 'white',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20
   },
+  button: {
+      backgroundColor: '#007bff',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+  },
+  buttonText: {
+      color: 'white',
+      fontSize: 16,
+      textAlign: 'center'
+  }
 });
